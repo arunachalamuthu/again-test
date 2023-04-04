d7b2692f438af36df57f53891f2a9083cc8b0455
arunachalamuthu <muthuvel171998@gmail.com>
2023-04-04 10:19:05 +0530

    ss

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express =require('express')
 const product =require('./api/product.js')
 const cors=require('cors')
+const path=require('path')
+
 const app=express()
 app.use(cors())
+
+// const public=path.resolve(__dirname,'/public')
+// app.use(public,express.static('/static'))
+
+app.use(express.static(path.join(__dirname+"/public/static")))
 app.use(express.json())
 
 const PORT=process.env.PORT || 5050
